3ff99a385c7947c76160910aec882052bd332688
Sarbani <paulsarbani39@gmail.com>
2025-01-23 13:17:19 +0530

    added snapshot tests, tag and annotation

--- e2e/tests/ui/addEmployee/addEmployee.spec.ts
+++ e2e/tests/ui/addEmployee/addEmployee.spec.ts
@@ -4,16 +4,80 @@ import LoginPage from '@pages/loginPage';
 import AddEmployeePage from '@pages/addEmployeePage';
 import { getRandomEmployeeDetails } from '@e2e/testdata/random';
 
-test('Add Employee', async ({ page }) => {
-  await page.goto(Env.BASE_URL);
-  const loginPage = new LoginPage(page);
-  const homePage = await loginPage.login(Env.USERNAME, Env.PASSWORD);
+test(
+  'Add Employee',
+  {
+    annotation: [
+      {
+        type: 'info',
+        description: 'Add employee with snapshots validation',
+      },
+    ],
+    tag: ['@releasev1.1.0'],
+  },
+  async ({ page }) => {
+    await page.goto(Env.BASE_URL);
+    await expect(page.locator('#app')).toMatchAriaSnapshot(`
+      - img "company-branding"
+      - heading "Login" [level=5]
+      - paragraph: "Username : Admin"
+      - paragraph: "Password : admin123"
+      - text:  Username
+      - textbox "Username"
+      - text:  Password
+      - textbox "Password"
+      - button "Login"
+      - paragraph: Forgot your password?
+      - link
+      - link
+      - link
+      - link
+      - paragraph: OrangeHRM OS 5.7
+      - paragraph:
+        - text: /© \\d+ - \\d+/
+        - link "OrangeHRM, Inc"
+        - text: . All rights reserved.
+      - img "orangehrm-logo"
+    `);
+    const loginPage = new LoginPage(page);
+    const homePage = await loginPage.login(Env.USERNAME, Env.PASSWORD);
+    await expect(page.getByLabel('Sidepanel')).toMatchAriaSnapshot(`
+    - textbox "Search"
+    - button ""
+    - separator
+    - list:
+      - listitem:
+        - link "Admin"
+      - listitem:
+        - link "PIM"
+      - listitem:
+        - link "Leave"
+      - listitem:
+        - link "Time"
+      - listitem:
+        - link "Recruitment"
+      - listitem:
+        - link "My Info"
+      - listitem:
+        - link "Performance"
+      - listitem:
+        - link "Dashboard"
+      - listitem:
+        - link "Directory"
+      - listitem:
+        - link "Maintenance"
+      - listitem:
+        - link "Claim":
+          - img
+      - listitem:
+        - link "Buzz"
+    `);
+    await homePage.getLeftMenuComponent().selectLeftMenuItem('PIM');
+    await homePage.getTopMenuComponent().selectTopMenuItem('Add Employee');
 
-  await homePage.getLeftMenuComponent().selectLeftMenuItem('PIM');
-  await homePage.getTopMenuComponent().selectTopMenuItem('Add Employee');
+    const addEmployeePage = new AddEmployeePage(page);
+    await addEmployeePage.addEmployee(getRandomEmployeeDetails());
 
-  const addEmployeePage = new AddEmployeePage(page);
-  await addEmployeePage.addEmployee(getRandomEmployeeDetails());
-
-  await expect(addEmployeePage.successMessage).toBeVisible();
-});
+    await expect(addEmployeePage.successMessage).toBeVisible();
+  }
+);
--- e2e/tests/ui/addEmployee/addEmployeeWithCustomFixture.spec.ts
+++ e2e/tests/ui/addEmployee/addEmployeeWithCustomFixture.spec.ts
@@ -2,8 +2,13 @@ import { test, expect } from '@e2e/fixtures/customFixtures';
 import { Env } from '@e2e/frameworkConfig/env';
 import { getRandomEmployeeDetails } from '@e2e/testdata/random';
 
-test('Add Employee', async ({ loginPage, homePage, addEmployeePage }) => {
+test('Add Employee validation with custom fixtures', async ({
+  loginPage,
+  homePage,
+  addEmployeePage,
+}) => {
   await loginPage.visit();
+
   await loginPage.login(Env.USERNAME, Env.PASSWORD);
   await homePage.getLeftMenuComponent().selectLeftMenuItem('PIM');
   await homePage.getTopMenuComponent().selectTopMenuItem('Add Employee');
